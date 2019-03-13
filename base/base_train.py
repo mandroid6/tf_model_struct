3577828bd62540156f04548acd5631e725077fbe
mandroid6 <razzormandar@gmail.com>
2019-03-13 09:48:25 +0530

    initial commit for base_train

---
+++ base/base_train.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+
+class BaseTrain:
+    def __init__(self):
+        pass
+    
+    def train(self):
+        pass
+    
+    def train_epoch(self):
+        pass
+    
+    def train_step(self):
+        pass
