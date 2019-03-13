d6995468edf659c19abad6f8f8d7c288986a6386
mandroid6 <razzormandar@gmail.com>
2019-03-13 09:31:29 +0530

    save and load checkpoints

--- base/base_model.py
+++ base/base_model.py
@@ -4,18 +4,24 @@ import tensorflow as tf
 
 # ensures that below methods are overided by every model
 class BaseModel:
-	def __init__(self):
+	def __init__(self, config):
 		self.config = config
         self.init_global_step()
         self.init_current_epoch()
         
 		
-	def save(self):
-		pass	
+	def save(self, sess):
+		print('Saving Model...")
+        self.saver.save(sess, self.config.checkpoint_dir, self.global_step_tensor)
+        print('Model Saved')
 		
-	def load(self):
-		pass
-		
+	def load(self, sess):
+		latest_ckpt = tf.train.latest_checkpoint(self.config.checkpoint_dir)
+		if latest_ckpt:
+            print('Loading model checkpoint {} ...\n'.format(latest_ckpt))
+            self.saver.restore(sess, latest_ckpt)
+            print('Model Loaded')
+            
 	def init_current_epoch(self):
 		with tf.variable_scope('cur_epoch'):
             self.cur_epoch_tensor = tf.Variable(0, trainable=False, name='cur_epoch')
